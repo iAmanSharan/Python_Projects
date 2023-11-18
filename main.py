d026486c02fcc5937eea4c236060bbd1b67f1d1a
iAmanSharan <102402525+iAmanSharan@users.noreply.github.com>
2023-11-18 07:45:00 +0000

    modfies

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 email_id = input('enter your email_id ')
-dot =  0
-rate = 0
-for i in range(len(email_id)):
-  if email_id[i]==".":
-    dot = i
-
-  if email_id[i] =="@":
-    rate = i
-    break
-username = email_id[dot+1:rate]
-domain_name=email_id[rate+1:]
-print(username," ",domain_name)
-
+def email_extractor(email_id):
+  dot =  0
+  rate = 0
+  for i in range(len(email_id)):
+    if email_id[i]==".":
+      dot = i
+  
+    if email_id[i] =="@":
+      rate = i
+      break
+  username = email_id[dot+1:rate]
+  domain_name=email_id[rate+1:]
+  print(username," ",domain_name)
+  
+  
